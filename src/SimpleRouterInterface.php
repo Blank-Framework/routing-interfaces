eaa273778e45475c0d927da2c36b580e3e34c794
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2026-04-02 22:31:43 +0200

    fix: Corrected return type (#3)

--- src/SimpleRouterInterface.php
+++ src/SimpleRouterInterface.php
@@ -3,8 +3,9 @@
 namespace BlankFramework\RoutingInterfaces;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 interface SimpleRouterInterface
 {
-    public function routeRequest(ServerRequestInterface $request): RouteInterface;
+    public function routeRequest(ServerRequestInterface $request): RequestHandlerInterface;
 }
