4fefe700453ad5e6bb7522058931e6f30dc3f23c
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2026-04-05 18:28:38 +0200

    feat: Added exceptions to the interface (#4)

--- src/SimpleRouterInterface.php
+++ src/SimpleRouterInterface.php
@@ -1,11 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BlankFramework\RoutingInterfaces;
 
+use BlankFramework\RoutingInterfaces\Exception\InvalidRouteException;
+use BlankFramework\RoutingInterfaces\Exception\RouteNotFoundException;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 interface SimpleRouterInterface
 {
+    /**
+     * @throws RouteNotFoundException
+     * @throws InvalidRouteException
+     */
     public function routeRequest(ServerRequestInterface $request): RequestHandlerInterface;
 }
